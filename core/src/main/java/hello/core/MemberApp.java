3187a4135974d2df1a1f053d2121e2c620ff6221
seungwon2 <dandelionsw2@ewhain.net>
2020-12-09 17:57:11 +0900

    :sparkles: 회원 도메인 개발 - 멤버 서비스 테스트

---
+++ core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        //새로운 멤버 만들어서 멤버 서비스에 넣기
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "A", Grade.VIP);
+        memberService.join(member);
+        //멤버 서비스에서 꺼내오기
+        Member findMember = memberService.findMember(1L);
+        //멤버 이름 비교
+        System.out.println("new member: " + member.getName());
+        System.out.println("find member: " + findMember.getName());
+
+    }
+}
