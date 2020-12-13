b973ca43b02623a22c272a39ea3e82e115ca93ba
seungwon2 <dandelionsw2@ewhain.net>
2020-12-13 11:55:18 +0900

    :sparkles: 새로운 정률 할인 정책 추가 및 테스트

---
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,35 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP면 10% 할인")
+    void vip_o() {
+        //given
+        Member member = new Member(1L, "VIPmember", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+    @Test
+    @DisplayName("VIP아니면 할인 없음")
+    void vip_x() {
+        //given
+        Member member = new Member(1L, "BASICmember", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
