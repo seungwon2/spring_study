85166def5713b268de482e2f68c032b5845a2842
seungwon2 <dandelionsw2@ewhain.net>
2020-12-15 09:35:28 +0900

    :sparkles: 싱글톤 적용 테스트 --> 항상 같은 객체 반환

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -22,4 +22,18 @@ public class SingletonTest {
         //싱글톤 없으니까 memberService 계속 생성
         Assertions.assertThat(memberService1).isNotEqualTo(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 적용")
+    void singletonContainer(){
+        //private으로 막아놔서 아래 코드 작동 못함
+        //new SingletonService();
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+    }
+
 }
