44a7d38da75b81a14931b43df70e3d2a8f792be7
seungwon2 <dandelionsw2@ewhain.net>
2020-12-15 10:25:22 +0900

    :sparkles: 스프링 singleton 문제해결, 무상태 설계로

--- core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -16,13 +16,10 @@ class StatefulServiceTest {
         StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
         StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
 
-        statefulService1.order("userA", 10000);
-        statefulService2.order("userB", 20000);
+        int userAPrice = statefulService1.order("userA", 10000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
-
-        //pirce가 private이고 한 객체를 공유해서 쓰니까 A의 주문 금액이 만원임에도 불구하고 마지막으로 적용된 B의 주문금액 20000원이 출력된다.
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+        System.out.println("price = " + userAPrice);
     }
 
     static class TestConfig{
